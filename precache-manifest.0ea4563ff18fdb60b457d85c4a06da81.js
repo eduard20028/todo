fa60e0128e86fbb50555cac31d936a727ef9b99c
Edward <edik45052002@mail.ru>
2020-02-24 01:09:39 +0200

    Updates

---
+++ precache-manifest.0ea4563ff18fdb60b457d85c4a06da81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e7161fe6235a2bc3dd53759871fb9c",
+    "url": "/eduard20028/todo/index.html"
+  },
+  {
+    "revision": "3df78b8cafe7d6a079b1",
+    "url": "/eduard20028/todo/static/css/main.60c58023.chunk.css"
+  },
+  {
+    "revision": "88ef087707047244ac20",
+    "url": "/eduard20028/todo/static/js/2.65dd199b.chunk.js"
+  },
+  {
+    "revision": "3df78b8cafe7d6a079b1",
+    "url": "/eduard20028/todo/static/js/main.5177158f.chunk.js"
+  },
+  {
+    "revision": "79b2fc72c77de5656eff",
+    "url": "/eduard20028/todo/static/js/runtime-main.c13199ae.js"
+  }
+]);
